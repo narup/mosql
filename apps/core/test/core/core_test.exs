3f8012fef66884ed70b5b05bc5e2b51b0829cea2
Puran Singh <puran@puransmkpro2021.myfiosgateway.com>
2022-05-11 23:53:03 -0400

    schema test

--- apps/core/test/core/core_test.exs
+++ apps/core/test/core/core_test.exs
@@ -1,5 +1,6 @@
 defmodule MS.CoreTest do
   use ExUnit.Case
+  require Logger
   doctest MS.Core
 
   test "greets the world" do
@@ -7,9 +8,25 @@ defmodule MS.CoreTest do
   end
 
   test "parse schema json" do
-    with {:ok, raw_json} <- File.read("fixtures/schema.json"),
-         {:ok, schema} <- Jason.decode(raw_json, [:atoms]) do
-      assert schema.collection == "users"
+    fp = Path.absname("test/fixtures/schema.json")
+    with {:ok, raw_json} <- File.read(fp),
+         {:ok, schema} <- Jason.decode(raw_json, [keys: :atoms]) do
+
+          assert schema.collection == "users"
+          assert schema.table == "user"
+
+          first_field = hd(schema.fields)
+          assert first_field.column == "id"
+
+      Logger.info("Collection: #{inspect(schema.collection)}")
+      Logger.info("Table: #{inspect(schema.table)}")
+      Logger.info("First field column: #{inspect(first_field.column)}")
+    else
+      {:error, :enoent} ->
+        IO.puts("Could not find schema.json")
+
+      :error ->
+        "something weird happened"
     end
   end
 end
